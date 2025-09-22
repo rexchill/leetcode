2f8c998e602b47c7dc95e8a8b5ab513d2fa5d076
rex <rexshell@gitcode.com>
2025-09-22 17:24:57 +0800

    T26:删除有序数组中的重复项

---
+++ leetcode150/t26.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// T26:删除有序数组中的重复项
+func removeDuplicates(nums []int) int {
+	left := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[left] {
+			left++
+			nums[left] = nums[i]
+		}
+	}
+	return left + 1
+}
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(nums), nums)
+
+}
