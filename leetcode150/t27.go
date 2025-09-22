c04083770f632a63b6d1bb5011a28795d1f8738f
rex <rexshell@gitcode.com>
2025-09-22 17:12:33 +0800

    T27:移除元素

---
+++ leetcode150/t27.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// T27:移除元素
+func removeElement(nums []int, val int) int {
+	left := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[left] = nums[i]
+			left++
+		}
+	}
+	return left
+}
+func main() {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	val := 2
+	fmt.Println(removeElement(nums, val), nums)
+}
